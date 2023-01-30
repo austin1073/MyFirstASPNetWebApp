518e6c29a6bb928d6792080802be472badb83662
austin <austinator007@rocketmail.com>
2023-01-30 10:23:35 -0700

    Add project files.

---
+++ MyFirstASPNetWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,13 @@
+﻿////document.getElementById("#btnSend").addEventListener("click", function () {
+////    alert("From: " + $("#txtFrom").val +
+////        ", Subject: " + $("#txtSubject").val +
+////        ", Message: " + $("#txtMessage").val);
+////})
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeToggle();
+})
